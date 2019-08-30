eb38fd469965bdc1ae56499eabe64d7d0ee58b3b
jiro4989 <jiroron666@gmail.com>
2019-08-30 19:45:23 +0900

    :wip: Add template

---
+++ action/shell/shell.go
@@ -0,0 +1,19 @@
+package shell
+
+import "io"
+
+func Add(w io.Writer, r io.Reader) error {
+	return nil
+}
+
+func Update(w io.Writer, r io.Reader) error {
+	return nil
+}
+
+func Disable(w io.Writer, r io.Reader) error {
+	return nil
+}
+
+func Remove(w io.Writer, r io.Reader) error {
+	return nil
+}
